095a3e85392b6ef948a8705e440ffa085d80e637
Naveed Latif <naveedlatifccw113@gmail.com>
2025-09-24 03:14:31 +0500

    fix routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Blood-Bank_Frontend">
       <AuthProvider>
         <App />
       </AuthProvider>
